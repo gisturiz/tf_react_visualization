8bf47e514a3f06826af1408e95629d75c4deefdd
Gustavo Isturiz <isturizgustavo@gmail.com>
2021-03-20 12:26:02 -0400

    app done and working well

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // Import dependencies
-import React, { useRef, useState, useEffect } from "react";
+import React, { useRef, useEffect } from "react";
 import * as tf from "@tensorflow/tfjs";
 import * as cocossd from "@tensorflow-models/coco-ssd";
 import Webcam from "react-webcam";
@@ -51,7 +51,7 @@ function App() {
     }
   };
 
-  useEffect(()=>{runCoco()},[]);
+  useEffect(()=>{runCoco()});
 
   return (
     <div className="App">
