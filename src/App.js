89d14c8bebc4c065218fdec588077b2e0c6b6c95
Gustavo Isturiz <isturizgustavo@gmail.com>
2021-03-20 12:33:32 -0400

    tf module edited

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // Import dependencies
 import React, { useRef, useEffect } from "react";
-import * as tf from "@tensorflow/tfjs";
+import "@tensorflow/tfjs";
 import * as cocossd from "@tensorflow-models/coco-ssd";
 import Webcam from "react-webcam";
 import "./App.css";
